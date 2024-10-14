83b516893c6a3e72bc7e5145dee8aa2b52d69048
fiqAsh <ashfiq625@gmail.com>
2024-10-15 00:55:02 +0600

    protectrpoute niddleware, conversation,message model,sebd.get message controller

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 //routes
 app.get("/", (req, res) => {
@@ -16,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(5000, () => {
 	connectToMongoDB();
