bb81ba1436acde5b33fd8e9064da9ff9076f7fb3
fiqAsh <ashfiq625@gmail.com>
2024-10-20 18:24:54 +0600

    implement socket io

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
+import { app, server } from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -23,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(5000, () => {
+server.listen(5000, () => {
 	connectToMongoDB();
 	console.log(`server is listening on ${PORT}`);
 });
