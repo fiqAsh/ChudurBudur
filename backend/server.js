8d00b0a403cd5ba50dd07acf8e01a62d3f2f3cc7
fiqAsh <ashfiq625@gmail.com>
2024-10-29 22:41:29 +0600

    depployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,13 +18,19 @@ app.use(express.json());
 app.use(cookieParser());
 
 //routes
-app.get("/", (req, res) => {
-	res.send("home route");
-});
+// app.get("/", (req, res) => {
+// 	res.send("home route");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(5000, () => {
 	connectToMongoDB();
